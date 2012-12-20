a75df495d7360142aa7867ed3235003c9dddefbc
grego <greg.faruch@gmail.com>
2012-12-20 13:59:30 +0100

    json event convertion ok

--- htdocs/axagenda/ajax/functions.php
+++ htdocs/axagenda/ajax/functions.php
@@ -1185,8 +1185,23 @@ function show_day_events($db, $day, $month, $year, $monthshown, $style, &$eventa
     /* print '</div>'."\n"; */
     }
 }
-dol_syslog("axagenda/ajax/functions.php EVENTARRAY LAST: ".json_encode($eventarray), LOG_DEBUG);
-echo translate_to_full_callendar(json_encode($eventarray));
+// dol_syslog("axagenda/ajax/functions.php EVENTARRAY LAST: ".json_encode($eventarray), LOG_DEBUG);
+	
+		/* array( */
+		/* 	'id' => 111, */
+		/* 	'title' => "Event1", */
+		/* 	'start' => "$year-$month-10", */
+		/* 	'url' => "http://yahoo.com/" */
+		/* ), */
 		
+		/* array( */
+		/* 	'id' => 222, */
+		/* 	'title' => "Event2", */
+		/* 	'start' => "$year-$month-20", */
+		/* 	'end' => "$year-$month-22", */
+		/* 	'url' => "http://yahoo.com/" */
+		/* ) */
+	
 
+echo json_encode(translate_to_full_callendar($eventarray));
 ?>
