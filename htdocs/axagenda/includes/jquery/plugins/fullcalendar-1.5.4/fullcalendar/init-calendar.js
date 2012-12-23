ca281c89ed685738cc45a9499e4da542426f2721
grego <greg.faruch@gmail.com>
2012-12-23 20:23:06 +0100

    remove pp js definition

--- htdocs/axagenda/includes/jquery/plugins/fullcalendar-1.5.4/fullcalendar/init-calendar.js
+++ htdocs/axagenda/includes/jquery/plugins/fullcalendar-1.5.4/fullcalendar/init-calendar.js
@@ -1,37 +1,37 @@
-function pp(object, depth, embedded) { 
-  typeof(depth) == "number" || (depth = 0)
-  typeof(embedded) == "boolean" || (embedded = false)
-  var newline = false
-  var spacer = function(depth) { var spaces = ""; for (var i=0;i<depth;i++) { spaces += "  "}; return spaces }
-  var pretty = ""
-  if (      typeof(object) == "undefined" ) { pretty += "undefined" }
-  else if ( typeof(object) == "boolean" || 
-            typeof(object) == "number" ) {    pretty += object.toString() } 
-  else if ( typeof(object) == "string" ) {    pretty += "\"" + object + "\"" } 
-  else if (        object  == null) {         pretty += "null" } 
-  else if ( object instanceof(Array) ) {
-    if ( object.length > 0 ) {
-      if (embedded) { newline = true }
-      var content = ""
-      for each (var item in object) { content += pp(item, depth+1) + ",\n" + spacer(depth+1) }
-      content = content.replace(/,\n\s*$/, "").replace(/^\s*/,"")
-      pretty += "[ " + content + "\n" + spacer(depth) + "]"
-    } else { pretty += "[]" }
-  } 
-  else if (typeof(object) == "object") {
-    if ( Object.keys(object).length > 0 ){
-      if (embedded) { newline = true }
-      var content = ""
-      for (var key in object) { 
-        content += spacer(depth + 1) + key.toString() + ": " + pp(object[key], depth+2, true) + ",\n" 
-      }
-      content = content.replace(/,\n\s*$/, "").replace(/^\s*/,"")
-      pretty += "{ " + content + "\n" + spacer(depth) + "}"
-    } else { pretty += "{}"}
-  }
-  else { pretty += object.toString() }
-  return ((newline ? "\n" + spacer(depth) : "") + pretty)
-}
+// function pp(object, depth, embedded) { 
+//   typeof(depth) == "number" || (depth = 0)
+//   typeof(embedded) == "boolean" || (embedded = false)
+//   var newline = false
+//   var spacer = function(depth) { var spaces = ""; for (var i=0;i<depth;i++) { spaces += "  "}; return spaces }
+//   var pretty = ""
+//   if (      typeof(object) == "undefined" ) { pretty += "undefined" }
+//   else if ( typeof(object) == "boolean" || 
+//             typeof(object) == "number" ) {    pretty += object.toString() } 
+//   else if ( typeof(object) == "string" ) {    pretty += "\"" + object + "\"" } 
+//   else if (        object  == null) {         pretty += "null" } 
+//   else if ( object instanceof(Array) ) {
+//     if ( object.length > 0 ) {
+//       if (embedded) { newline = true }
+//       var content = ""
+//       for each (var item in object) { content += pp(item, depth+1) + ",\n" + spacer(depth+1) }
+//       content = content.replace(/,\n\s*$/, "").replace(/^\s*/,"")
+//       pretty += "[ " + content + "\n" + spacer(depth) + "]"
+//     } else { pretty += "[]" }
+//   } 
+//   else if (typeof(object) == "object") {
+//     if ( Object.keys(object).length > 0 ){
+//       if (embedded) { newline = true }
+//       var content = ""
+//       for (var key in object) { 
+//         content += spacer(depth + 1) + key.toString() + ": " + pp(object[key], depth+2, true) + ",\n" 
+//       }
+//       content = content.replace(/,\n\s*$/, "").replace(/^\s*/,"")
+//       pretty += "{ " + content + "\n" + spacer(depth) + "}"
+//     } else { pretty += "{}"}
+//   }
+//   else { pretty += object.toString() }
+//   return ((newline ? "\n" + spacer(depth) : "") + pretty)
+// }
 
 
 
@@ -82,6 +82,9 @@ $(document).ready(function() {
 	// eventDragStop: function(event) {
 	//     alert("eventDragStop event <"+pp(even)+">");
 	// },
+	eventClick: function(calEven, jsEvent, view) {
+	    window.location = "/comm/action/fiche.php?id="+calEven.id;
+	},
 	
 	eventDrop: function(event, dayDelta, minuteDelta, allDay, revertFunc, jsEvent, ui, view) {
 /*	    // update original url: http://dolibarr-new.local:88/comm/action/fiche.php/comm/action/fiche.php
